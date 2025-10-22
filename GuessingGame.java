06b9beb2c0e6b88d142a608ca8ffc6e7f9322129
Mahad <ac75239@skola.goteborg.se>
2025-10-22 19:13:40 +0200

    add main class GuessingGame

---
+++ GuessingGame.java
@@ -0,0 +1,14 @@
+
+public class GuessingGame{
+
+   public static void main(String[] args){
+
+         Guesser game = new Guesser(0, 1000); // Skapar ett Guesser objekt som kan gissa tal mellan 0-1000
+
+          game.start(); // Startar spelet
+
+
+
+
+}
+}
